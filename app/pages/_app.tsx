c9fb348d909d7707c1ac0d8da1667626cc04d5e6
Arghun-dev <arghun.developer@gmail.com>
2021-03-11 08:14:28 +0330

    I converted my files to Typescript

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,11 +1,8 @@
 import '../styles/globals.css'
-import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />  
-    </Layout>
+    <Component {...pageProps} />  
   ) 
 }
 
